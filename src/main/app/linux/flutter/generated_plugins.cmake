1719c865a44025b2e1b9315f8612d5e898efd59a
office-pc nix root <paul@fauchon.dev>
2026-02-21 11:57:00 +0800

    update dependencies

--- src/main/app/linux/flutter/generated_plugins.cmake
+++ src/main/app/linux/flutter/generated_plugins.cmake
@@ -5,10 +5,9 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   dynamic_color
   file_saver
+  flutter_secure_storage_linux
   oidc_linux
-  simple_secure_storage_linux
   url_launcher_linux
-  webcrypto
   window_to_front
 )
 
